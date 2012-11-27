5327902d33f93243edad68535d34b15ad9c50407
Alessandro Di Maria <adm@m42.ch>
2012-11-28 00:04:50 +0100

    world json

--- server/application.rb
+++ server/application.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   send_file "#{settings.root}/index.html", :type => 'text/html'
+end
+
+get '/world' do
+  content_type :json
+  World.new(11).create.to_json
 end
 
 get '/test' do
---
+++ server/world.rb
@@ -0,0 +1,49 @@
+class World
+  include Enumerable
+
+  def initialize columns, rows = nil
+    rows = columns unless rows
+    @matrix = Array.new(columns) {Array.new(rows)}
+  end
+
+  def create
+    set_each_field do |field|
+      fib(rand(7))
+    end
+  end
+
+  def each_field_with_index
+    @matrix.each_with_index do |column, x|
+      column.each_with_index {|row, y| yield row, x, y}
+    end
+  end
+
+  def set_each_field
+    @matrix.each_with_index do |column, y|
+      column.each_with_index {|row, x| @matrix[x][y] = yield}
+    end
+  end
+
+  def set_each_field_with_index
+    @matrix.each_with_index do |column, x|
+      column.each_with_index {|row, y| @matrix[x][y] = yield x, y}
+    end
+  end
+
+  # 0 1 2 3 5 8 13
+  def fib(n)
+    return 0 if n == 0
+    return 1 if n == 1
+    return 2 if n == 2
+    return fib(n-1) + fib(n-2)
+  end
+
+  def each_field
+    @matrix.each do |column|
+      column.each {|row| yield row}
+    end
+  end
+
+  alias each each_field
+
+end
