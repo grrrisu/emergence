f39c22160bf7c350cd08ce84591e8c5f0c408047
Alessandro Di Maria <adm@m42.ch>
2012-11-27 18:44:51 +0100

    add jasmine

--- server/application.rb
+++ server/application.rb
@@ -4,3 +4,7 @@ require 'sinatra/reloader'
 get '/' do
   send_file "#{settings.root}/index.html", :type => 'text/html'
 end
+
+get '/test' do
+  send_file "#{settings.root}/public/spec/SpecRunner.html", :type => 'text/html'
+end
---
+++ server/public/spec/unit/foo_spec.js
@@ -0,0 +1,10 @@
+describe("foo", function() {
+
+  beforeEach(function() {
+  });
+
+  it("should have api caller", function(){
+    expect(true).toBe(true);
+  });
+
+});
