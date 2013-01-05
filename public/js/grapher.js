533fdf3730d2acaa13583ab9831105a28048fc14
Alessandro Di Maria <adm@m42.ch>
2013-01-05 22:09:41 +0100

    fix count  preloaded images

--- public/js/grapher.js
+++ public/js/grapher.js
@@ -1,14 +1,17 @@
 var Grapher = function(width, height){
 
-  var image_sources = {
-    '0_desert': 'images/0_desert4.png',
-    '1_grass': 'images/1_grass4.png',
-    '2_grass': 'images/2_grass4.png',
-    '3_grass': 'images/3_grass4.png',
-    '5_grass': 'images/5_grass4.png',
-    '8_forest': 'images/8_forest4.png',
-    '13_forest': 'images/13_forest4.png'
-  };
+  var self = this,
+      image_sources = {
+        '0_desert': 'images/0_desert4.png',
+        '1_grass': 'images/1_grass4.png',
+        '2_grass': 'images/2_grass4.png',
+        '3_grass': 'images/3_grass4.png',
+        '5_grass': 'images/5_grass4.png',
+        '8_forest': 'images/8_forest4.png',
+        '13_forest': 'images/13_forest4.png'
+      };
+
+  this.stage = null;
 
   this.init = function(){
     this.preload_images(image_sources, this.render);
@@ -18,10 +21,17 @@ var Grapher = function(width, height){
   this.preload_images = function(sources, callback){
     var images = {};
     var loadedImages = 0;
+    var numImages = 0;
+    // get num of sources
+    for(var src in sources) {
+      numImages++;
+    }
     for(var src in sources) {
       images[src] = new Image();
       images[src].onload = function() {
-        if(++loadedImages >= sources.length) {
+        console.log(loadedImages, numImages);
+        if(++loadedImages >= numImages) {
+          console.log('callback');
           callback(images);
         }
       };
@@ -30,7 +40,7 @@ var Grapher = function(width, height){
   };
 
   this.render = function(){
-    this.stage = new Kinetic.Stage({
+    self.stage = new Kinetic.Stage({
       container: 'prawns',
       width: width,
       height: height
