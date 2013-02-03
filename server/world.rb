c6cba5d9d36e832673f3a8d1dc67975a0b846b7e
Alessandro Di Maria <adm@m42.ch>
2013-02-03 14:46:02 +0100

    world builder

--- server/world.rb
+++ server/world.rb
@@ -1,20 +1,13 @@
 require_relative '../ludo/matrix'
+require_relative 'builders/world_builder'
 
 class World < Ludo::Matrix
 
   def create
-    set_each_field do |field|
-      {vegetation: fib(rand(4)+3)}
-    end
+    options = {}
+    builder = WorldBuilder.new(self)
+    builder.create(options)
     self
-  end
-
-  # 0 1 2 3 5 8 13
-  def fib(n)
-    return 0 if n == 0
-    return 1 if n == 1
-    return 2 if n == 2
-    return fib(n-1) + fib(n-2)
   end
 
 end
